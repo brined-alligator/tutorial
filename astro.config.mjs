5faedde234104040b75f40c3bc71565328ad77ac
brined-alligator <ivan.khoinguyen@gmail.com>
2025-10-22 19:00:47 +0700

    Changes to be committed: 	modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/Greeting.jsx 	modified:   tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://fanciful-llama-1a3463.netlify.app",
+  integrations: [preact()]
+});
+
+//-
